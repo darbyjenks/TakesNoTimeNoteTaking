0ad210baa10edfb7c44de8e6d0bfc7a7541c84fc
Darby Johnson <darbyjjohnson1@gmail.com>
2022-03-18 13:23:29 -0400

    updated the save and api routes to have a try and catch

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -4,24 +4,35 @@ const fs = require('fs');
 
 //API get request
 router.get("/notes", (req, res) => {
-    fs.readFile("./db/db.json", "utf8", (err, data) =>{
-        if (err) throw err;
-        res.json(JSON.parse(data));
-    });
+    try {
+        fs.readFile("./db/db.json", "utf8", (err, data) =>{
+            if (err) throw err;
+            res.json(JSON.parse(data));
+        }); 
+    } catch (error) {
+        res.json(error).status(500)
+    }
 });
 
 //API post request
 router.post("/notes", (req, res) =>{
-    fs.readFile("./db/db.json", "utf8", (err, data) => {
-        if (err) throw err;
-        let yourNote = JSON.parse(data);
-        if(yourNote){
-            yourNote.push(req.body);
-            fs.writeFile("./db/db.json", JSON.stringify(yourNote), (err) => {
-                if(err) return err;
-            });
-        }
-    });
+    try {
+        fs.readFile("./db/db.json", "utf8", (err, data) => {
+            if (err) throw err;
+            let yourNote = JSON.parse(data);
+            if(yourNote){
+                yourNote.push(req.body);
+                fs.writeFile("./db/db.json", JSON.stringify(yourNote), (err) => {
+                    if(err) return err;
+                });
+            }
+        });
+        res.json({
+            msg: "Success"
+        })
+    } catch (error) {
+        res.json(error)
+    }
 });
 
 // router.delete('/notes/:id', (req, res) => {
